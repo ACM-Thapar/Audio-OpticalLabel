8bb0781fc49ad85620f16f08ea7a797f8ad9a64e
jinik21 <nikheeljain@outlook.com>
2021-02-19 14:40:42 +0530

    added decoding part

--- ClientApp/FlaskApp/app.py
+++ ClientApp/FlaskApp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, url_for,render_template
+from flask import Flask, request, url_for,render_template,send_file
 from werkzeug.utils import secure_filename
 import os
 
@@ -8,6 +8,7 @@ from v2.plotter import plotDatapoints as plt
 import v2.AudioToID as AudioID
 import v2.IDToHash as Hash
 import v2.hashReducer as hr
+from v2.v2ImageToSubSign import imageToSubSignature as decoder
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm.attributes import flag_modified
 
@@ -27,11 +28,13 @@ class Label(db.Model):
     AID=db.Column('AID',db.Text)
     SubSignature=db.Column('SubSignature',db.Text)
     label_loc=db.Column('label_loc',db.Text)
-    def _init_(self,ID,AID,SubSignature,label_loc):
+    audio_loc=db.Column('audio_loc',db.Text)
+    def _init_(self,ID,AID,SubSignature,label_loc,audio_loc):
         self.ID=ID
         self.AID=AID
         self.SubSignature=SubSignature
         self.label_loc=label_loc
+        self.audio_loc=audio_loc
 
 
 def allowed_file(filename):
@@ -80,7 +83,7 @@ def predict():
         Labelgen(loc2, 120,locsave)
 
         plt(locsave, locsave, v2Sign)
-        label_temp=Label(AID=aid,SubSignature=v2Sign,label_loc=locsave)
+        label_temp=Label(AID=aid,SubSignature=v2Sign,label_loc=locsave,audio_loc=loc1)
         print(label_temp)
         db.session.add(label_temp)
         db.session.commit()
@@ -90,6 +93,30 @@ def predict():
             return render_template('error.html',pos='not found aid')
         print(loc1,loc2)
     return render_template('result.html',aid=lb.AID,sign=lb.SubSignature,locsave=lb.label_loc)
+
+@app.route('/decode',methods=['POST'])
+def decode():
+    if request.method== 'POST':
+        try:
+            if 'check_image' not in request.files:
+                return jsonify({"Message":"send an image to decode"})
+            img=request.files['check_image']
+            if img and allowed_file(img.filename) :
+                filename = secure_filename(img.filename)
+                img.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                loc_check=os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            
+            subsigns=decoder(loc_check)
+            for subsign in subsigns:
+                lb=db.session.query(Label).filter_by(SubSignature=subsign).first()
+                if lb != None:
+                    break
+            if lb != None:
+                return send_file(lb.audio_loc,mimetype="audio/wav",as_attachment=True,attachment_filename="audio.wav")
+            else:
+                return jsonify({"Message":"Not Found in Database"})
+        except:
+            return jsonify({"Message":"Oops Something Went Wrong"})
 
 if __name__ == "__main__":
     app.run(debug=True)
